011f482475ef0926c3f27bd680766f794e5f5a96
digitalsolution360global <ds360host@gmail.com>
2026-03-02 14:51:31 +0530

    done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,10 @@
 import AboutClientPage from "./aboutClient";
 
+export const metadata = {
+  title: "Our Medical Services | Raj Nursing Home Hapur – Gynae, Laparoscopy, Ultrasound & More",
+  description: "Explore a wide range of medical services at Raj Nursing Home Hapur, including gynecology, laparoscopic surgery, ultrasound, maternity care, pharmacy, and 24x7 emergency services. Comprehensive and affordable care under one roof.",
+};
 
-function page() {
-  return (
-    <AboutClientPage/>
-  )
+export default function Page() {
+  return <AboutClientPage />;
 }
-
-export default page
